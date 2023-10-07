9af5849f8cb89df6aea949340d09121cc617143f
Patryk Gonet <patryk.gonet@microsoft.wsei.edu.pl>
2023-10-07 12:25:46 +0200

    Add reversed loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int[] table = { 1, 2, 3, 4, 5, };
+            int[] table = { 1, 2, 3, 4, 5 };
             string[] tableString = { "Ala", "ma", "kota" };
 
             for(int i = 0; i < table.Length; i++)
@@ -17,7 +17,16 @@
                 Console.WriteLine(tableString[i]);
             }
 
-            foreach(int i in table) Console.WriteLine(i);
+            foreach(int i in table)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = table.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(table[i]);
+            }
         }
+       
     }
 }   
