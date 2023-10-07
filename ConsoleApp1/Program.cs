1a87c091da1b784a7efbe8880c5978dd6c6be868
Patryk Gonet <patryk.gonet@microsoft.wsei.edu.pl>
2023-10-07 12:18:06 +0200

    Add tables and writing them in loops

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] table = { 1, 2, 3, 4, 5, };
+            string[] tableString = { "Ala", "ma", "kota" };
 
+            for(int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine(table[i]);
+            }
 
-            Console.WriteLine("Ala ma kota");
+            for(int i = 0; i < tableString.Length; i++)
+            {
+                Console.WriteLine(tableString[i]);
+            }
+
+            foreach(int i in table) Console.WriteLine(i);
         }
     }
 }   
